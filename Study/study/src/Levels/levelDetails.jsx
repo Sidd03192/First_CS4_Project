e7bdce00e5f95457a265dee4bc90e652bedcd0fd
Sidd03192 <135279087+Sidd03192@users.noreply.github.com>
2023-12-11 00:57:46 -0600

    final week

--- Study/study/src/Levels/levelDetails.jsx
+++ Study/study/src/Levels/levelDetails.jsx
@@ -1,7 +1,7 @@
 // LevelDetailPage.jsx
-import Box from '@mui/material/Box';
+import ConfettiExplosion from 'react-confetti-explosion';
 import TextField from '@mui/material/TextField';
-import {Input} from "@nextui-org/react";
+import { Progress } from 'semantic-ui-react'
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import {
@@ -9,24 +9,26 @@ import {
   collection,
   doc,
   updateDoc,
-  serverTimestamp,
-  getDoc,
-  increment,
+  getDoc, setDoc,
 } from 'firebase/firestore';
 import { db } from '../firebase';
 import { getAuth } from 'firebase/auth';
-import AddLevelForm from './AddLevelForm';
-import { Alert } from '@mui/material';
+import { Alert, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 import './Levels.css';
 
 const LevelDetailPage = () => {
-
   const { levelId } = useParams();
   const [user, setUser] = useState(getAuth().currentUser);
   const [levels, setLevels] = useState([]);
   const [questions, setQuestions] = useState([]);
-  const [userProgress, setUserProgress] = useState();
+  const [userProgress, setUserProgress] = useState(0);
+  const [showAlert, setShowAlert] = useState(false);
+  const [showCompletionAlert, setShowCompletionAlert] = useState(false);
+  const [showErrorAlert, setShowErrorAlert] = useState(false);
+  const [isConfettiActive, setIsConfettiActive] = useState(false);
 
+const navigate = useNavigate();
   useEffect(() => {
     const fetchLevels = async () => {
       try {
@@ -46,140 +48,180 @@ const LevelDetailPage = () => {
 
     const fetchQuestions = async () => {
       try {
-        console.log("attempting to fetch questions !")
-        // Assuming you have a 'questions' collection under each level document
         const questionsCollection = collection(db, 'levels', levelId, 'questions');
         const questionsSnapshot = await getDocs(questionsCollection);
-    
+
         const questionsData = questionsSnapshot.docs.map((doc) => ({
           id: doc.id,
           ...doc.data(),
         }));
-    
+
         setQuestions(questionsData);
-        console.log("here are the questions "+ questionsData);
-        console.log ("the first question is " +questions[0]);
       } catch (error) {
         console.error('Error fetching questions:', error);
       }
     };
+
     
 
-    const fetchUserProgress = async () => {
-      try {
-        if (user) {
-          const userRef = doc(db, 'users', user.uid);
-          const levelProgressRef = doc(userRef, 'progress', levelId);
-          const levelProgressSnapshot = await getDoc(levelProgressRef);
-
-          if (levelProgressSnapshot.exists()) {
-            setUserProgress(levelProgressSnapshot.data().correct); // Set to correct answers directly
-          } else {
-            setUserProgress(0);
-          }
-        }
-      } catch (error) {
-        console.error('Error fetching user progress:', error);
-      }
-    };
     fetchLevels();
     fetchQuestions();
     fetchUserProgress();
   }, [levelId, user]);
+  const fetchUserProgress = async () => {
+    try {
+      if (user) {
+        const userRef = doc(db, 'users', user.uid);
+        const levelProgressRef = doc(userRef, 'progress', levelId);
+        const levelProgressSnapshot = await getDoc(levelProgressRef);
 
+        if (levelProgressSnapshot.exists()) {
+          setUserProgress(levelProgressSnapshot.data().correct);
+        } else {
+          setUserProgress(0);
+        }
+      }
+    } catch (error) {
+      console.error('Error fetching user progress:', error);
+    }
+  };
   const currentLevel = levels.find((level) => level.id === levelId);
   const currentQuestionIndex = userProgress;
 
   const handleAnswer = async (userAnswer) => {
     try {
       const currentQuestion = questions[currentQuestionIndex];
-  
-      // Check if the user's answer is correct
+
       const isCorrect = userAnswer === currentQuestion.answer;
-  
-      // Handle the user's answer (e.g., update progress in Firestore)
+
       if (isCorrect) {
-        // Update user progress in Firestore
         if (user) {
           const userRef = doc(db, 'users', user.uid);
           const levelRef = doc(userRef, 'progress', levelId);
-  
-          // Get the current progress data
+
           const progressSnapshot = await getDoc(levelRef);
           const currentProgress = progressSnapshot.data();
-  
-          // Update the "correct" field for the current level
+
           await updateDoc(levelRef, {
             correct: currentProgress.correct + 1,
           });
-  
-          console.log('User progress updated successfully.');
-  
-          // Set the user's progress to the total number of correct answers
-          setUserProgress(currentProgress.correct);
-  
-          // Move to the next question if available
-          if (currentQuestionIndex + 1 < questions.length) {
-            setUserProgress(userProgress + 1);
-          } else {
-            // Handle moving to the next level or completion logic
-            console.log('User has completed all questions for this level');
-          }
+
+          // Set confetti active for 1 second
+          setIsConfettiActive(true);
+          setTimeout(() => {
+            setIsConfettiActive(false);
+
+            // Show the alert after confetti animation is complete
+            setShowAlert(true);
+            setTimeout(() => {
+              setShowAlert(false);
+
+              if (currentQuestionIndex + 1 < questions.length) {
+                setUserProgress(userProgress + 1);
+              } else {
+                setShowCompletionAlert(true);
+              }
+            }, 1000);
+          }, 1000);
         }
       } else {
+        setShowErrorAlert(true);
+        setTimeout(() => {
+          setShowErrorAlert(false);
+        }, 1000);
         console.log('Incorrect answer. Please try again.');
-        // You can handle incorrect answers here (e.g., display a message)
       }
     } catch (error) {
       console.error('Error handling answer:', error);
     }
   };
-  
-  
-  
-  
-  
+
+  const resetProgress = async () => {
+    console.log("Attempting to reset progress");
+
+    try {
+      if (user) {
+        const userRef = doc(db, 'users', user.uid);
+        const levelProgressRef = doc(userRef, 'progress', levelId);
+
+        // Reset the user's progress for the current level to 0
+        await setDoc(levelProgressRef, { correct: 0 });
+
+        // Fetch the updated progress to ensure it's reflected immediately
+        await fetchUserProgress();
+
+        // Refresh the screen to start the user back on question 1
+        window.location.reload();
+      }
+    } catch (error) {
+      console.error('Error resetting progress:', error);
+    }
+  };
+
+
+
+
 
   return (
     <div className='currentquestion'>
-      <Alert />
+      {/* Show confetti when isConfettiActive is true */}
+      {showAlert && <Alert severity="success">Great Job !!</Alert>}
+      {isConfettiActive && <ConfettiExplosion /> }
+
+      
+      {showCompletionAlert && (
+        <div>
+          <Alert
+            severity="success"
+            action={
+              <Button color="inherit" size="small" onClick={() => navigate('/learn')}>
+                Levels
+              </Button>
+            }
+          >
+            Level Completed! {/* Customize the message as needed */}
+          </Alert>
+          <button onClick={resetProgress} className="button type1"> </button>
+        </div>
+      )}
+      {showErrorAlert && <Alert severity="error">Incorrect answer. Please try again.</Alert>}
+
       <h1 className='thislevel'>{currentLevel && `${currentLevel.id}`}</h1>
       {user && (
         <>
-          <p className=''>Lets set started, {user.displayName}!</p>
+          <p className=''>Let's get started, {user.displayName}!</p>
           <p className='progress'>Your progress: {userProgress} / {questions.length}</p>
         </>
       )}
-      {questions.length > 0 & userProgress < questions.length && (
-  <div>
-    <p className='thequestion'>{questions[userProgress].question}</p>
-    <TextField className='answer'
-      
-      id='filled-basic' variant='filled'
-      type="text" 
-      placeholder="Type your answer"
-      value={questions[userProgress].userAnswer || ''}
-      onChange={(e) => {
-        const updatedQuestions = [...questions];
-        updatedQuestions[userProgress] = {
-          ...updatedQuestions[userProgress],
-          userAnswer: e.target.value,
-        };
-        setQuestions(updatedQuestions);
-      }}
-    />
-    <button class="answerbutton" onClick={() => handleAnswer(questions[userProgress].userAnswer)}>
-      Submit Answer
-    </button>
-  </div>
-)}
+      {questions.length > 0 && userProgress < questions.length && (
+        <div>
+          <p className='thequestion'>{questions[userProgress].question}</p>
+          <TextField
+            className='answer'
+            id='filled-basic'
+            variant='filled'
+            type="text"
+            placeholder="Type your answer"
+            value={questions[userProgress].userAnswer || ''}
+            onChange={(e) => {
+              const updatedQuestions = [...questions];
+              updatedQuestions[userProgress] = {
+                ...updatedQuestions[userProgress],
+                userAnswer: e.target.value,
+              };
+              setQuestions(updatedQuestions);
+            }}
+          />
+          <button className="answerbutton" onClick={() => handleAnswer(questions[userProgress].userAnswer)}>
+            Submit Answer
+          </button>
+        </div>
+      )}
 
+      <Progress className="progressbar" percent={(userProgress/questions.length)*100 +5} indicating />
 
-
-      
     </div>
   );
 };
 
 export default LevelDetailPage;
-
